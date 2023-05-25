973daa204a5b6333910cc04891b685565662af95
Waynexcellence <waynexcellence@gmail.com>
2023-05-25 10:33:15 +0800

    linux

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 all:
-	gcc ship.c -D USER_INPUT -o user_ship
-	gcc ship.c -D PROGRAM_INPUT -o program_ship
+	gcc ship.c -D USER_INPUT             -o user_ship
+	gcc ship.c -D PROGRAM_INPUT          -o program_ship
+	gcc ship.c -D MONTE -D PROGRAM_INPUT -o monte_ship
+	gcc test.c -D USER_INPUT             -o test_ship
 clean:
 	rm -f *~~
-	rm -f user_ship program_ship
+	rm -f user_ship program_ship monte_ship test_ship
--- ship.c
+++ ship.c
@@ -15,16 +15,19 @@
 #define m2_3_ALL_POSSIBLE_TYPE 306
 #define m1_4_ALL_POSSIBLE_TYPE 156
 
-#define P_abcd (double)1200/7776
-#define P_abcde (double)240/7776
+#define m_abcd_TYPE 1200
+#define m_abcde_TYPE 240
+
+#define p_ooooo (double)16697056/362797056   // ~ 0.046023 (exact) monte = 0.046135 , 6^11 = 362797056
+#define p_abcde (double)542243500/2176782336 // ~ 0.249103 (exact) monte = 0.248804 , 6^12 = 2176782336
 
 #define m2_3_AVERAGE (double)(m2_3_ALL_POSSIBLE_SCORE)/(double)(m2_3_ALL_POSSIBLE_TYPE)
 #define m1_4_AVERAGE (double)(m1_4_ALL_POSSIBLE_SCORE)/(double)(m1_4_ALL_POSSIBLE_TYPE)
 #define mALL_AVERAGE (double)(TOTAL_DICE)*3.5
 
-#define mALL_PARAMETER (1)
-#define m2_3_PARAMETER (1)
-#define m1_4_PARAMETER (1)
+#define mALL_PARAMETER (double)(1.2)
+#define m2_3_PARAMETER (double)(1)
+#define m1_4_PARAMETER (double)(1)
 
 #define SUM(n) (1+n)*n/2
 #define SUMSUM(n) (n)*(n+1)*(n+2)/6
@@ -37,36 +40,31 @@
 }
 #define NOTICE(string) printf("\e[1;46m[ NOTICE ] %s\e[0m \n" , string );
 
-//                            Yah  1_4        abcde 2_3 abcd         all
-int miss_order[12] = { 1 , 2 , 12 , 9 , 3 , 4 , 11 , 8 , 10 , 5 , 6 , 7 };// parameter can change
-int miss_index = 0;
+//                                 Yah  1_4        abcde 2_3 abcd         all
+int miss_order[13] = { -1 , 1 , 2 , 12 , 9 , 3 , 4 , 11 , 8 , 10 , 5 , 6 , 7 };// parameter can change
+int miss_index = 1;
+int test_order[13] = { -1 , 1 , 2 , 3 , 7 , 4 , 5 , 6 , 10 , 8 , 9 , 11 , 12 };
 
 typedef struct Sheet{
 	bool filled[13];
 	int score[13];
 }Sheet;
 
-typedef struct Consideration{
-	int score[13];
-	bool need[13];
-}Consideration;
-
 int C_m_n( int m , int n ){
-	int result = 1;
-	if( n > m/2 ) n = m-n;
-	for(int x=m-n+1;x<=m;x++) result *= x;
-	for(int x=2;x<=n;x++)     result /= x;
-	return result;
+	if( m < 0 || n < 0 ) ERR();
+	int ret = 1;
+	n = (n>m/2)? m-n : n;
+	for(int x=m-n+1;x<=m;x++) ret *= x;
+	for(int x=2;x<=n;x++)     ret /= x;
+	return ret;
 }
 int power6( int n ){
-	if( n == 0 ) return 1;
-	if( n == 1 ) return 6;
-	if( n == 2 ) return 36;
-	if( n == 3 ) return 216;
-	if( n == 4 ) return 1296;
-	if( n == 5 ) return 7776;
-	ERR();
-	return -1;
+	if( n > 11 ) ERR();
+	int ret = 1;
+	for(int x=1;x<=n;x++){
+		ret *= 6;
+	}
+	return ret;
 }
 int sum( int level , int n ){
 	if( level == -1 )     return 1;
@@ -78,7 +76,7 @@ int sum( int level , int n ){
 }
 void get_amount( int dice[TOTAL_DICE] , int amount[7] ){
 	memset( amount , 0 , 7*sizeof(int) );
-	for(int x=0;x<5;x++){
+	for(int x=0;x<TOTAL_DICE;x++){
 		amount[ dice[x] ] ++;
 		amount[0] += dice[x];
 	}
@@ -117,30 +115,13 @@ bool is_abcde( int amount[7] ){
 	if( amount[1] || amount[6] ) return true;
 	return false;
 }
-bool is_ooooo( int dice[TOTAL_DICE] ){
-	for(int x=1;x<5;x++){
-		if( dice[x] != dice[x-1] ) return false;
+bool is_ooooo( int amount[7] ){
+	for(int x=1;x<=6;x++){
+		if( amount[x] != 0 && amount[x] != TOTAL_DICE ) return false;
 	}
 	return true;
 }
 
-void add_score_sheet( int dice[TOTAL_DICE] , Sheet *score_sheet , int index ){
-	int amount[7] = {};
-	get_amount( dice , amount );
-	if( index > 12 || index < 1 || score_sheet->filled[index] ) ERR();
-	if( 1 <= index && index <= 6 ){
-		score_sheet->score[0]    += amount[index] * index; // [0] stores the current sum of number 1~6
-		score_sheet->score[index] = amount[index] * index;
-	}
-	if( index == 7 )               score_sheet->score[index] = amount[0];
-	if( index == 8 )               score_sheet->score[index] = (is_2_3(amount))?  amount[0] : 0;
-	if( index == 9 )               score_sheet->score[index] = (is_1_4(amount))?  amount[0] : 0;
-	if( index == 10 )              score_sheet->score[index] = (is_abcd(amount))?        15 : 0;
-	if( index == 11 )              score_sheet->score[index] = (is_abcde(amount))?       30 : 0;
-	if( index == 12 )              score_sheet->score[index] = (is_ooooo(dice))?         50 : 0;
-	score_sheet->filled[index] = true;
-	return ;
-}
 void show_dice( int mode , int dice[TOTAL_DICE] , bool strategy[TOTAL_DICE] ){
 	char output[5][256] = {};
 	int length = TOTAL_DICE*7;
@@ -151,7 +132,7 @@ void show_dice( int mode , int dice[TOTAL_DICE] , bool strategy[TOTAL_DICE] ){
 	}
 	memset( output[0] , 'b' , length );
 	memset( output[4] , 'b' , length );
-	if( mode == 1 ) NOTICE("[ Notice ] Yellow means re throw again.");
+	if( mode == 1 ) NOTICE("Yellow means re throw again.");
 	for(int x=0;x<5;x++){
 		for(int y=0;y<length;y++){
 			if( strategy[y/7] ){
@@ -237,45 +218,66 @@ void recur_dice_index( int ret[TOTAL_DICE] , int num_dice , int level ){
 	}
 }
 
-int score_index_get( int amount[7] , int index ){
-	if( index <= 6 ) return amount[index]*index;
-	if( index <= 9 ) return amount[0];
-	if( index <= 11 ) return 15*(index-9);
-	if( index == 12 ) return 50;
-	ERR();
-}
-void score_want_get( int dice[TOTAL_DICE] , Sheet score_sheet , int ret[2] ){ // ret[0] is the score , ret[1] is the index
-	ret[0] = -1;
-	ret[1] = -1;
+int score_index_get( int dice[TOTAL_DICE] , int index ){
 	int amount[7] = {};
 	get_amount( dice , amount );
+	if( index <= 6 ) return amount[index]*index;
+	if( index == 7 ) return amount[0];
+	if( index == 8 ) return ( is_2_3(amount) )? amount[0] : 0;
+	if( index == 9 ) return ( is_1_4(amount) )? amount[0] : 0;
+	if( index == 10 ) return ( is_abcd(amount) )? 15 : 0;
+	if( index == 11 ) return ( is_abcde(amount) )? 30 : 0;
+	if( index == 12 ) return ( is_ooooo(amount) )? 50 : 0;
+	ERR();
+}
+bool examine_over_lower_bound( int score , int index ){
+	if( index <= 6 ){
+		if( score >= (TOTAL_DICE+1)/2*index ) return true;
+	}
+	else if( index == 7 ){
+		if( score >= mALL_AVERAGE*mALL_PARAMETER ) return true;
+	}
+	else if( index == 8 ){
+		if( score >= m2_3_AVERAGE*m2_3_PARAMETER ) return true;
+	}
+	else if( index == 9 ){
+		if( score >= m1_4_AVERAGE*m1_4_PARAMETER ) return true;
+	}
+	else if( index <=12 ){
+		if( score ) return true;
+	}
+	else ERR();
+	return false;
+}
+void score_want_get( int dice[TOTAL_DICE] , Sheet score_sheet , int ret[2] ){ // ret[0] is the score , ret[1] is the index
+	memset( ret , 0 , 2*sizeof(int) );
 	while( score_sheet.filled[miss_order[miss_index]] ){
 		miss_index ++;
+		assert( miss_index <= 12 );
 	}
-	int miss_score = score_index_get( amount , miss_order[miss_index] );
+	int index_get = -1;
 	for(int x=1;x<=12;x++){
-		int index_get = score_index_get(amount,x);
-
-		if( score_sheet.filled[x] ) continue;
-		if( x <= 6 && amount[x] < HALF_DICE ) continue;
-		if( index_get < ret[0] ) continue;
-
-		if( x == 7 && (index_get<=mALL_AVERAGE*mALL_PARAMETER) ) continue;
-		if( x == 8 && (index_get<=m2_3_AVERAGE*m2_3_PARAMETER || !is_2_3(amount)) ) continue;
-		if( x == 9 && (index_get<=m1_4_AVERAGE*m1_4_PARAMETER || !is_1_4(amount)) ) continue;
-		if( x == 10 && (!is_abcd(amount)) ) continue;
-		if( x == 11 && (!is_abcde(amount)) ) continue;
-		if( x == 12 && (!is_ooooo(dice)) ) continue;
-		ret[0] = index_get;
-		ret[1] = x;
+		int test_index = test_order[x];                // from most possible to least possible
+		if( score_sheet.filled[test_index] ) continue; // the score has been filled
+		int score_test_index_get = score_index_get( dice , test_index );
+		if( examine_over_lower_bound( score_test_index_get , test_index ) ) index_get = test_index;
 	}
-	if( ret[1] == -1 ){
-		ret[0] = miss_score;
-		ret[1] = miss_order[miss_index];
+	ret[0] = score_index_get( dice , (index_get == -1)? miss_order[miss_index]:index_get );
+	ret[1] = (index_get == -1)? miss_order[miss_index]:index_get;
+	if( score_sheet.filled[ret[1]] ){
+		printf("ret[1] = %d index_get = %d miss_index = %d\n" , ret[1] , index_get , miss_index );
+		ERR();
 	}
 }
+void add_score_sheet( int dice[TOTAL_DICE] , Sheet *score_sheet , int index ){
+	if( score_sheet->filled[index] ) ERR();
+	score_sheet->score[index] = score_index_get( dice , index );
+	score_sheet->filled[index] = true;
+	if( index <= 6 ) score_sheet->score[0] += score_sheet->score[index];
+	return ;
+}
 
-void get_strategy( int dice[5] , bool strategy[5] , Sheet score_sheet ){ // true -> throw again
+void get_strategy( int dice[TOTAL_DICE] , bool strategy[TOTAL_DICE] , Sheet score_sheet ){ // true -> throw again
 	int temp_dice[5] = {};
 	double **expected_score = calloc( 6 , sizeof(double*) ); // re throw 0~5 dices
 	for(int x=0;x<=TOTAL_DICE;x++){
@@ -285,6 +287,7 @@ void get_strategy( int dice[5] , bool strategy[5] , Sheet score_sheet ){ // true
 	int re_throw_index[5] = {};
 	int ret[2] = {};
 	score_want_get( dice , score_sheet , ret );
+	// if( ret[1] == 7 ) ret[0] = 0;
 	expected_score[0][0] = ret[0];
 	for(int x=1;x<=TOTAL_DICE;x++){ // x means the amount of dices re thrown
 		int choose = C_m_n( TOTAL_DICE , x ); // choose also equals to the length of expected_score[x]
@@ -331,95 +334,115 @@ void get_strategy( int dice[5] , bool strategy[5] , Sheet score_sheet ){ // true
 
 int main(){
     srand( time(0) );
-    // 35 1 2 3 4 5 6 all 2_3 1_4 abcd abcde ooooo
+	#ifdef MONTE
+	double try_sum = 0;
+	int try_times = 5000;
+	for(int try=0;try<try_times;try++){
+	#endif 
+
     Sheet score_sheet = {};
     int dice[TOTAL_DICE] = {};
+	memset( &score_sheet , 0 , sizeof(Sheet) );
+	miss_index = 1;
 
     for(int x=0;x<12;x++){
-		printf("\e[1;46m[ NOTICE ] %d round\e[0m \n" , x+1 );
+		#ifndef MONTE
+			printf("\e[1;46m[ NOTICE ] %d round\e[0m \n" , x+1 );
+		#endif
 		bool strategy[TOTAL_DICE] = {};
 		int result[2] = {};
-		// ************************************************** first input and show
+		// ************************************************** first input
         #ifdef USER_INPUT
 			NOTICE("Please enter the value you throw.");
 			scanf("%d %d %d %d %d" , &dice[0] , &dice[1] , &dice[2] , &dice[3] , &dice[4] );
-        #elif defined PROGRAM_INPUT
+        #elif PROGRAM_INPUT
 			for(int y=0;y<TOTAL_DICE;y++){
 				dice[y] = rand()%6+1;
 			}
         #endif
-		show_dice( 0 , dice , strategy );
-		sleep(BREAK_TIME);
-
-		// ************************************************** first strategy and show
+		// ************************************************** first show
+		#ifndef MONTE
+			show_dice( 0 , dice , strategy );
+			sleep(BREAK_TIME);
+		#endif
+		// ************************************************** first strategy
 		get_strategy( dice , strategy , score_sheet );
-		show_dice( 1 , dice , strategy );
-
-		// **************************************************
-		NOTICE("2 chance remain to throw.");
-		sleep(BREAK_TIME);
-
-		// ************************************************** second input and show
+		#ifndef MONTE
+			show_dice( 1 , dice , strategy );
+			NOTICE("2 chance remain to throw.");
+			sleep(BREAK_TIME);
+		#endif
+		// ************************************************** second input
 		#ifdef USER_INPUT
 			NOTICE("Please enter the value you throw.");
 			scanf("%d %d %d %d %d" , &dice[0] , &dice[1] , &dice[2] , &dice[3] , &dice[4] );
-        #elif defined PROGRAM_INPUT
+        #elif PROGRAM_INPUT
 			for(int y=0;y<TOTAL_DICE;y++){
 				if( strategy[y] ) dice[y] = rand()%6+1;
 			}
         #endif
+		// ************************************************** second show
 		memset( strategy , 0 , TOTAL_DICE*sizeof(bool) );
-		show_dice( 0 , dice , strategy );
-		sleep(BREAK_TIME);
-
-		// ************************************************** second strategy and show
+		#ifndef MONTE
+			show_dice( 0 , dice , strategy );
+			sleep(BREAK_TIME);
+		#endif
+		// ************************************************** second strategy
 		get_strategy( dice , strategy , score_sheet );
-		show_dice( 1 , dice , strategy );
-
-		// **************************************************
-		NOTICE("1 chance remain to throw.");
-		sleep(BREAK_TIME);
-
-		// ************************************************** third input and show
+		#ifndef MONTE
+			show_dice( 1 , dice , strategy );
+			NOTICE("1 chance remain to throw.");
+			sleep(BREAK_TIME);
+		#endif
+		// ************************************************** third input
 		#ifdef USER_INPUT
 			NOTICE("Please enter the value you throw.");
 			scanf("%d %d %d %d %d" , &dice[0] , &dice[1] , &dice[2] , &dice[3] , &dice[4] );
-        #elif defined PROGRAM_INPUT
+        #elif PROGRAM_INPUT
 			for(int y=0;y<TOTAL_DICE;y++){
 				if( strategy[y] ) dice[y] = rand()%6+1;
 			}
         #endif
+		// ************************************************** third show
 		memset( strategy , 0 , TOTAL_DICE*sizeof(bool) );
-		show_dice( 0 , dice , strategy );
-		sleep(BREAK_TIME);
-		// **************************************************
+		#ifndef MONTE
+			show_dice( 0 , dice , strategy );
+			sleep(BREAK_TIME);
+		#endif
+		// ************************************************** calculate the optimal
 		score_want_get( dice , score_sheet , result );
-		printf("\e[1;46mindex: %d is the program wants to select.\e[0m \n" , result[1] );
-		printf("\e[1;46mAnd it should get %d score.\e[0m \n" , result[0] );
-
-		// ************************************************** only calculate which index should select
+		#ifndef MONTE
+			printf("\e[1;46mindex: %d is the program wants to select.\e[0m \n" , result[1] );
+			printf("\e[1;46mAnd it should get %d score.\e[0m \n" , result[0] );
+		#endif
+		// ************************************************** fourth input index
 		#ifdef USER_INPUT
 			int index = 0;
 			show_score_sheet( score_sheet );
 			NOTICE("Please enter the index you want to select.");
 			scanf("%d" , &index );
 			add_score_sheet( dice , &score_sheet , index );
-        #elif defined PROGRAM_INPUT
+        #elif PROGRAM_INPUT
 			add_score_sheet( dice , &score_sheet , result[1] );
         #endif
-		show_score_sheet( score_sheet );
-
 		// **************************************************
-		#ifdef PROGRAM_INPUT
-			sleep(5);
+		#ifndef MONTE
+			show_score_sheet( score_sheet );
+			sleep(3);
 		#endif
     }
 	if( score_sheet.score[0] >= 63 ) score_sheet.score[0] += 35;                   // Bonus
 	for(int x=7;x<=12;x++)           score_sheet.score[0] += score_sheet.score[x]; // Sum the total score to [0]
-	NOTICE("Your total score is ");
-	printf("\e[1;46m %d \e[0m \n" , score_sheet.score[0] );
-	memset( &score_sheet , 0 , sizeof(Sheet) );
+	printf("\e[1;46m Your total score is %d \e[0m \n" , score_sheet.score[0] );
+	#ifndef MONTE
+	show_score_sheet( score_sheet );
+	#endif
 
+	#ifdef MONTE
+	try_sum += score_sheet.score[0];
+	}
+	printf("you try play %d times, and total score is %d , average is %lf\n", try_times , (int)try_sum , (double)try_sum/try_times );
+	#endif
 
     return 0;
 }
